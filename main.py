44d2453acde465aafac2cea6d2d38a2aebf5c1ea
reversearrow <reversearrow@gmail.com>
2016-09-20 20:16:44 -0400

    Update

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import webapp2
+import webapp2,jinja2,os
+
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+template = JINJA_ENVIRONMENT.get_template('newpost.html')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, Udacity!')
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+class NewPost(webapp2.RequestHandler):
+	def get(self):
+		self.response.write(template.render())
+
+	def post(self):
+		self.response.write(template.render())
+
+
+
+app = webapp2.WSGIApplication([('/', MainPage),('/newpost', NewPost)], debug=True)
