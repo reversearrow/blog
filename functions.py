5032ffdb232b10a965de2fcf545cdf1048c17e57
Jay Shah <reversearrow@gmail.com>
2016-10-01 14:06:19 -0400

    Updating Functions

--- functions.py
+++ functions.py
@@ -1,4 +1,23 @@
+import cgi,re
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile("^.{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+
 def verify(text):
 	if text and not text.isspace():
 		return True
 		
+def valid_username(username):
+    return USER_RE.match(username)
+
+def valid_password(password):
+    return PASSWORD_RE.match(password)
+
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+def verify_password(password,verify):
+    if password == verify:
+        return True
+    else:
+        return False
