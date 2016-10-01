7f233537c4c56291b767a6c2f84bbdabc8ba06a0
Jay Shah <reversearrow@gmail.com>
2016-10-01 14:04:37 -0400

    Adding Signup From Unit 2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import webapp2,jinja2,os,functions,datetime,urllib2,urllib,time
+from functions import valid_username, valid_password, valid_email,verify_password
 from google.appengine.ext import db
+
+user_signup_form = """
+<h2>User Signup</h2>
+<form method="POST">
+<label> Username <input type = "text" name = "username" value = "%(username)s"></input></label> <br>
+<label> Password <input type = "password" name = "password" value = ""> </input></label> <br>
+<label> Verify <input type = "password" name = "verify"> </input></label> <br>
+<label> Email <input type = "text" name = "email" value = "%(email)s"> </input></label> <br>
+<input type="submit">
+<h2> %(error)s </h2>
+"""
+
+welcome_message = """
+<h1> Welcome %(username)s.</h1>
+"""
+
 
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 
@@ -57,8 +74,49 @@ class Blog(Handler):
 		if query:
 			self.render("blog.html",subject=query.subject,created=query.created,content=query.content)
 
+class Signup(webapp2.RequestHandler):
+	def write_form(self,username="",password="",email="",error=""):
+		self.response.write(user_signup_form % ({"username":username,"password":password,"email":email,"error":error}))
 
-app = webapp2.WSGIApplication([('/blog/newpost', NewPost),('/blog',Blog),webapp2.Route('/blog/<keyid:[a-zA-Z0-9-_]+>',handler=Blog,handler_method="permalink")], debug=True)
+	def get(self):
+		self.write_form()
+
+	def validate_input(self,username,password,verify,email):
+		if not valid_username(username):
+			error = "'%s' is not a valid username." % (username)
+			self.write_form(username,password,email,error)
+		elif not valid_password(password):
+			error = "Password is not valid is not a valid password."
+			self.write_form(username,password,email,error)
+		elif not verify_password(password,verify):
+			error = "Verify and Password doesn't match"
+			self.write_form(username,password,email,error)
+		elif not valid_email(email):
+			error = "'%s' is not an valid email." % (email)
+			self.write_form(username,password,email,error)
+		else:
+			self.redirect("/unit2/welcome?username=" + username)
+
+	def post(self):
+		username = self.request.get("username")
+		password = self.request.get("password")
+		verify = self.request.get("verify")
+		email = self.request.get("email")
+
+		self.validate_input(username,password,verify,email)
+
+
+class Welcome(webapp2.RequestHandler):
+	def get(self):
+		username = self.request.get("username")
+		if valid_username(username):
+			self.response.out.write(welcome_message % ({"username": username}))
+		else:
+			self.redirect("/unit2/signup")
+
+
+
+app = webapp2.WSGIApplication([('/blog/newpost', NewPost),('/blog',Blog),('/blog/signup',Signup),("/blog/welcome",Welcome),webapp2.Route('/blog/<keyid:[a-zA-Z0-9-_]+>',handler=Blog,handler_method="permalink")], debug=True)
 
 
 
